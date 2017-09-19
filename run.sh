45a325f6894e48e985ffdc140b69360f19fe4c62
Daisuke Ito <ito@wacul.co.jp>
2017-09-19 12:55:50 +0900

    remove trailing newline character

--- run.sh
+++ run.sh
@@ -74,6 +74,6 @@ export AWS_ACCESS_KEY_ID="${WERCKER_ECR_ADDTAG_KEY}"
 export AWS_SECRET_ACCESS_KEY="${WERCKER_ECR_ADDTAG_SECRET}"
 export AWS_DEFAULT_REGION="${WERCKER_ECR_ADDTAG_REGION}"
 
-TEMP_FILE=$(mktemp)
-aws ecr batch-get-image --registry-id "${WERCKER_ECR_ADDTAG_AWS_REGISTRY_ID}" --repository-name "${WERCKER_ECR_ADDTAG_REPOSITORY}" --image-ids "imageTag=${WERCKER_ECR_ADDTAG_SOURCE_TAG}" --query images[].imageManifest --output text > "${TEMP_FILE}" && aws ecr put-image --registry-id "${WERCKER_ECR_ADDTAG_AWS_REGISTRY_ID}" --repository-name "${WERCKER_ECR_ADDTAG_REPOSITORY}" --image-tag "${WERCKER_ECR_ADDTAG_ADD_TAG}" --image-manifest "file://${TEMP_FILE}"
+IMAGE_MANIFEST=$(aws ecr batch-get-image --registry-id "${WERCKER_ECR_ADDTAG_AWS_REGISTRY_ID}" --repository-name "${WERCKER_ECR_ADDTAG_REPOSITORY}" --image-ids "imageTag=${WERCKER_ECR_ADDTAG_SOURCE_TAG}" --query images[].imageManifest --output text|head -c -1)
+aws ecr put-image --registry-id "${WERCKER_ECR_ADDTAG_AWS_REGISTRY_ID}" --repository-name "${WERCKER_ECR_ADDTAG_REPOSITORY}" --image-tag "${WERCKER_ECR_ADDTAG_ADD_TAG}" --image-manifest "${IMAGE_MANIFEST}"
 
